afa80d38544823d4a6008d88c68aee2bc3629094
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2024-01-30 22:29:30 -0500

    Updated tooltip

--- src/components/mapView.js
+++ src/components/mapView.js
@@ -3,7 +3,24 @@ import * as echarts from "echarts";
 
 const MapView = () => {
     const option = {
-        tooltip: {},
+        tooltip: {
+            trigger: 'item',
+            position: 'right',
+            borderColor: '#000000',
+            textStyle: {
+                font: 'Poppins',
+                fontSize: '13px',
+                fontWeight: '400',
+                lineHeight: '18px',
+                letterSpacing: '-0.01em',
+                color: '#286067',
+            },
+            extraCssText: "border-radius: 8px;",
+            formatter: function (params) {
+                console.log(params);
+                return '<span>' + params.data[2] + ' enrolled</span>'
+            },
+        },
         geo: {
           tooltip: {
             show: true
@@ -20,7 +37,7 @@ const MapView = () => {
           geoIndex: 0,
           zlevel: 1,
           symbolSize: function (params) {
-            return params[2]
+            return params[2]/2
           },
           itemStyle: {
             color: '#187C85',
@@ -30,14 +47,15 @@ const MapView = () => {
             tooltip: 2
           },
           data: [
-            [160, 97, 10],
-            [100, 570, 10],
+            [160, 97, 36],
+            [100, 570, 131],
             [200, 300, 20],
             [300, 443, 17],
             [400, 200, 31],
             [400, 400, 20],
             [500, 500, 20],
             [700, 500, 18],
+            [850, 120, 18],
           ],
           symbolKeepAspect: true,
         }
@@ -56,7 +74,7 @@ const MapView = () => {
 
     return (
         <div>
-            <div style={{ width: "100%", height: "50vh" }} id='beef'></div>
+            <div style={{ width: '1000px', height: '770px' }} id='beef'></div>
         </div>
     )
 }
